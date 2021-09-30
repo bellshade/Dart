a2179b0829b815d2a3a55f335b1b6745d443ddfd
Z Alviandy R <38882629+zalviandyr@users.noreply.github.com>
2021-09-30 12:47:59 +0700

    Menambah materi Operator dan Kondisi (#2)
    * add: menambahkan materi operator
    
    * add: menambahkan materi kondisi
    
    * add: switch case di materi kondisi

---
+++ dartlang/bin/5_kondisi/kondisi.dart
@@ -0,0 +1,95 @@
+main(List<String> args) {
+  print('Operator Perbandingan');
+  print('1. Operator Setara');
+  int a = 5;
+  int b = 5;
+  String word = 'kata';
+  print(a == b);
+  print(a == word);
+
+  print('2. Operator Tidak Setara');
+  print(a != b);
+  print(a != word);
+
+  print('3. Operator Lebih Dari');
+  a = 5;
+  b = 6;
+  print(a > b);
+  print(b > a);
+
+  print('4. Operator Kurang Dari');
+  print(a < b);
+  print(b < a);
+
+  print('5. Operator Lebih Dari atau Setara');
+  a = 8;
+  b = 6;
+  print(a >= b);
+  b = 8;
+  print(a >= b);
+
+  print('5. Operator Kurang Dari atau Setara');
+  a = 8;
+  b = 6;
+  print(a <= b);
+  b = 8;
+  print(a <= b);
+
+  print('\n----------------------');
+  print('Operator Logika');
+  print('1. Operator Negasi');
+  bool kondisi = true;
+  print(!kondisi);
+
+  print('2. Operator OR');
+  print(true || true);
+  print(true || false);
+  print(false || false);
+
+  print('3. Operator AND');
+  print(true && true);
+  print(true && false);
+  print(false && false);
+
+  print('\n----------------------');
+  print('Ekespresi Bersyarat');
+  print('1. Ternary Operator');
+  int angka = 5;
+  // jika angka 5 habis dibagi 2 maka bilangan tersebut Genap
+  String bilangan = (angka % 2) == 0 ? 'Genap' : 'Ganjil';
+  print(bilangan);
+
+  print('2. Null Safety Operator');
+  // tanda ? pada tipe data menandakan bahwa variable tersebut bisa menyimpan nilai null
+  String? kata;
+  print(kata ?? 'Tidak boleh null');
+
+  print('3. If Else');
+  int nilai = 6;
+  if (nilai < 6) {
+    // jika nilai kurang dari 6
+    print('Remedial');
+  } else if (nilai >= 6 && nilai <= 8) {
+    // jika nilai lebih sama dari 6 dan kurang sama dari 8
+    print('Cukup');
+  } else {
+    // jika nilai lebih dari 8
+    print('Sempurna');
+  }
+
+  print('4. Switch Case');
+  String waktu = 'Malam';
+  switch (waktu) {
+    case 'Pagi':
+      print('Selamat Pagi');
+      break;
+    case 'Siang':
+      print('Selamat Siang');
+      break;
+    case 'Malam':
+      print('Selamat Malam');
+      break;
+    default:
+      print('Hai untuk mu');
+  }
+}
