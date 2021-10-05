ba088a5a88c76c9b1396f68213f12b036d3c2bb9
athallahmaajid <66192387+athallahmaajid@users.noreply.github.com>
2021-10-05 07:54:45 +0700

    fix: operator dan logika aritmatika documentation in code (#9)

--- lib/basic/4_operator_dan_logika_aritmatika/operator_dan_logika_aritmatika.dart
+++ lib/basic/4_operator_dan_logika_aritmatika/operator_dan_logika_aritmatika.dart
@@ -5,76 +5,94 @@ class Operator {
     int a = 5;
     int b = 5;
     int hasil = a + b;
-    print(hasil);
+    print("a + b: " + hasil.toString());
+
+    print('\n');
 
     print('2. Kurang');
     a = 10;
     b = 7;
     hasil = a - b;
-    print(hasil);
+    print("a - b: " + hasil.toString());
+
+    print('\n');
 
     print('3. Kali');
     a = 3;
     b = 4;
     hasil = a * b;
-    print(hasil);
+    print("a * b: " + hasil.toString());
+
+    print('\n');
 
     print('4. Bagi');
     a = 5;
     b = 3;
     double hasilBagi = a / b;
-    print(hasilBagi);
+    print("a / b: " + hasilBagi.toString());
+
+    print('\n');
 
     print('5. Bagi, tapi integer');
     a = 5;
     b = 3;
     hasil = a ~/ b;
-    print(hasil);
+    print("a ~/ b: " + hasil.toString());
+
+    print('\n');
 
     print('6. Modulo');
     a = 7;
     b = 3;
     hasil = a % b;
-    print(hasil);
+    print("a % b: " + hasil.toString());
 
-    print('\n---------------------------');
+    print('\n');
+
+    print('\n===========================');
     print('Increment dan Decrement');
     print('1. Prefix Increment');
     a = 0;
     b = ++a;
-    print(a);
-    print(b);
+    print("a:" + a.toString());
+    print("b:" + b.toString());
+
+    print('\n');
 
     print('2. Postfix Increment');
     a = 0;
     b = a++;
-    print(a);
-    print(b);
+    print("a: " + a.toString());
+    print("b: " + b.toString());
+
+    print('\n');
 
     print('3. Prefix Decrement');
     a = 0;
     b = --a;
-    print(a);
-    print(b);
+    print("a: " + a.toString());
+    print("b: " + b.toString());
+
+    print('\n');
 
     print('4. Postfix Decrement');
     a = 0;
     b = a--;
-    print(a);
-    print(b);
+    print("a: " + a.toString());
+    print("b: " + b.toString());
 
-    print('\n---------------------------');
+    print('\n===========================');
     print('Assignment Operator');
     a = 5;
     a += 10;
-    print(a);
+    print("a: " + a.toString());
 
     a = 7;
     a ~/= 3;
-    print(a);
+    print("a: " + a.toString());
 
     a = 7;
     a %= 3;
-    print(a);
+    print("a: " + a.toString());
   }
 }
