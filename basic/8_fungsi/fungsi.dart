e6408858eda740bb6f668861ff6531d1de954f02
Alvin Zulham <50919691+VyscoZyza@users.noreply.github.com>
2021-10-07 10:51:47 +0700

    add: menambahkan penjelasan pemanggilan fungsi

--- basic/8_fungsi/fungsi.dart
+++ basic/8_fungsi/fungsi.dart
@@ -13,20 +13,23 @@ void perkenalan(String nama) {
 main() {
   print("Perkenalan");
 
-  //menerima input pengguna berupa tahun lahir
+  //menerima input pengguna berupa tahun lahir dan disimpan di variabel tahun
   stdout.write("Input Tahun Lahir : ");
   int? tahun = int.parse(stdin.readLineSync()!);
 
-  //menerima input nama pengguna
+  //menerima input nama pengguna dan disimpan di variabel n
   stdout.write("Input Nama : ");
   String? n = stdin.readLineSync()!;
 
   //garis pembatas
   print("==============");
-  // memanggil fungsi hitungUsia
+
+  // memanggil fungsi hitungUsia dengan parameter tahun (tahun lahir yang diinputkan pengguna) dan akan mengembalikan nilai dengan tipe data int dan disimpan di variabel usia
   int usia = hitungUsia(tahun);
+  
+  // memanggil fungsi perkenalan dengan parameter n (nama yang diinputkan pengguna)
   perkenalan(n);
 
-  //mencetak usia
+  //mencetak nilai usia
   print("Umur saya $usia");
 }
