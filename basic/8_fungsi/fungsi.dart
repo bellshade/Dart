ea9070d413b381831ddac1066ae73972b5ab38c1
amdev142 <hirarkyid@gmail.com>
2022-01-01 05:40:41 +0700

    feat: tambah kode dari dokumentasi

--- basic/8_fungsi/fungsi.dart
+++ basic/8_fungsi/fungsi.dart
@@ -39,27 +39,27 @@ void pembagian3({required int nilai1, required int nilai2}) {
 }
 
 main() {
-  // print('Perkenalan');
+  print('Perkenalan');
 
-  // //menerima input pengguna berupa tahun lahir dan disimpan di variabel tahun
-  // stdout.write('Input Tahun Lahir : ');
-  // int? tahun = int.parse(stdin.readLineSync()!);
+  //menerima input pengguna berupa tahun lahir dan disimpan di variabel tahun
+  stdout.write('Input Tahun Lahir : ');
+  int? tahun = int.parse(stdin.readLineSync()!);
 
-  // //menerima input nama pengguna dan disimpan di variabel n
-  // stdout.write('Input Nama : ');
-  // String? n = stdin.readLineSync()!;
+  //menerima input nama pengguna dan disimpan di variabel n
+  stdout.write('Input Nama : ');
+  String? n = stdin.readLineSync()!;
 
-  // //garis pembatas
-  // print('==============');
+  //garis pembatas
+  print('==============');
 
-  // // memanggil fungsi hitungUsia dengan parameter tahun (tahun lahir yang diinputkan pengguna) dan akan mengembalikan nilai dengan tipe data int dan disimpan di variabel usia
-  // int usia = hitungUsia(tahun);
+  // memanggil fungsi hitungUsia dengan parameter tahun (tahun lahir yang diinputkan pengguna) dan akan mengembalikan nilai dengan tipe data int dan disimpan di variabel usia
+  int usia = hitungUsia(tahun);
 
-  // // memanggil fungsi perkenalan dengan parameter n (nama yang diinputkan pengguna)
-  // perkenalan(n);
+  // memanggil fungsi perkenalan dengan parameter n (nama yang diinputkan pengguna)
+  perkenalan(n);
 
-  // //mencetak nilai usia
-  // print('Umur saya $usia');
+  //mencetak nilai usia
+  print('Umur saya $usia');
   
   // Positional Parameter
   pembagian1(4, 2);
